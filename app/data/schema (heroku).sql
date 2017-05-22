5be4014cfbfe7cc22ca093ff39338b2c61c0d4eb
John A.T. Nguyen <johnatnguyen@gmail.com>
2017-05-21 19:52:34 -0700

    5/21/17 @ 7:51pm

--- app/data/schema (heroku).sql
+++ app/data/schema (heroku).sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
 	id INT auto_increment NOT NULL,
     name VARCHAR(50),
     photo VARCHAR(200),
-    scores VARCHAR(50),
+    scores VARCHAR(100),
     PRIMARY KEY(id)
 );
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+
 var connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -16,41 +17,92 @@ connection.connect(function(err) {
     console.log('connected as id ' + connection.threadId);
 });
 
+var friends;
+var user;
+
 module.exports = function(app) {
     app.get('/api/friends', function(req, res) {
         connection.query('SELECT * FROM users', function(err, data) {
-            res.json(data);
+            friends = data;
+            res.json(friends);
         });
     });
 
     app.post('/api/friends', function(req, res) {
-        console.log(req.body.name);
-        console.log(req.body.photo);
-        console.log(req.body.scores);
-
-        // connection.query('INSERT INTO users SET ?', {
-        //     name: req.body.name,
-        //     photo: req.body.photo,
-        //     scores: req.body.scores
-        // }, function(err, result) {
-        //     if (err) throw err;
-        // });
-
-        // connection.query('INSERT INTO users (name, photo, scores) VALUES (?, ?, ?)', [req.body.name, req.body.photo, req.body.scores], function(err, result) {
-        //     if (err) throw err;
-        // });
-
-        // connection.query('INSERT INTO users (name, photo, scores) VALUES ('req.body.name', 'req.body.photo', 'req.body.scores')', function(err, result) {
-        //     if (err) throw err;
-        // });
-
         connection.query('SELECT * FROM users', function(err, data) {
-            if (data.length) {
-            	// compatibility logic
-            } else {
-            	// WOULD THIS WORK???
-                res.send(alert('You are the first user!'));
+            friends = data;
+
+            user = req.body;
+            userScores = user.scores;
+
+            var totalArray = [];
+
+            for (var i = 0, n = friends.length; i < n; i++) {
+                var total = 0;
+                for (var j = 0, o = JSON.parse(friends[i].scores).length; j < o; j++) {
+                    total += Math.abs(parseInt(userScores[j]) - parseInt(JSON.parse(friends[i].scores)[j]));
+                }
+                totalArray.push(total);
             }
+
+            var lowestIndex = 0;
+
+            for (var i = 1, n = totalArray.length; i < n; i++) {
+                if (totalArray[i] <= totalArray[lowestIndex]) {
+                    lowestIndex = i;
+                }
+            }
+
+            res.json(friends[lowestIndex]);
+            
+            connection.query('INSERT INTO users SET ?', {
+                name: req.body.name,
+                photo: req.body.photo,
+                scores: JSON.stringify(req.body.scores)
+            }, function(err, result) {
+                if (err) throw err;
+            });
+
+            // res.json(friends[lowestIndex]);
+            // if (friends.filter(function(friend) { friend.name === req.body.name }).length = 0) {
+            //     connection.query('INSERT INTO users SET ?', {
+            //         name: req.body.name,
+            //         photo: req.body.photo,
+            //         scores: JSON.stringify(req.body.scores)
+            //     }, function(err, result) {
+            //         if (err) throw err;
+            //     });
+            // }
+
+            // if (friends.filter(function(friend) { friend.name === req.body.name }).length > 0) {
+            //     res.json(friends[lowestIndex]);
+            // } else {
+            //     res.json(friends[lowestIndex]);
+            //     connection.query('INSERT INTO users SET ?', {
+            //         name: req.body.name,
+            //         photo: req.body.photo,
+            //         scores: JSON.stringify(req.body.scores)
+            //     }, function(err, result) {
+            //         if (err) throw err;
+            //     });
+            // }
+
+            // console.log(totalArray.join());
+            // console.log(Math.min(totalArray.join()));
+            // console.log(totalArray.indexOf(Math.min(totalArray.join())));
+
+            // connection.query('INSERT INTO users (name, photo, scores) VALUES (?, ?, ?)', [req.body.name, req.body.photo, req.body.scores], function(err, result) {
+            //     if (err) throw err;
+            // });
+
+            // connection.query('INSERT INTO users (name, photo, scores) VALUES ('
+            //     req.body.name ', '
+            //     req.body.photo ', '
+            //     req.body.scores ')',
+            //     function(err, result) {
+            //         if (err) throw err;
+            //     });
+
         });
     });
 }
